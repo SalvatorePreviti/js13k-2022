4cc797d211dae92d8452084f09b1b09eec916c4d
Salvatore Previti <roorback@gmail.com>
2022-06-17 00:42:26 +0100

    minor

--- app/dev-tools/index.ts
+++ app/dev-tools/index.ts
@@ -9,13 +9,15 @@ export function debug_checkShaderCompileStatus(
   shader: WebGLShader,
   info: DebugReportInfo,
 ): void {
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error("error", gl.getShaderInfoLog(shader) || "compilation failed", info);
-  } else {
-    const infoLog = gl.getShaderInfoLog(shader);
-    if (infoLog) {
-      if (infoLog.indexOf("WARN") >= 0) {
-        console.warn("warn", gl.getShaderInfoLog(shader), info);
+  if (DEBUG) {
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.error("error", gl.getShaderInfoLog(shader) || "compilation failed", info);
+    } else {
+      const infoLog = gl.getShaderInfoLog(shader);
+      if (infoLog) {
+        if (infoLog.indexOf("WARN") >= 0) {
+          console.warn("warn", gl.getShaderInfoLog(shader), info);
+        }
       }
     }
   }
@@ -26,23 +28,25 @@ export function debug_checkShaderProgramLinkStatus(
   shaderProgram: WebGLProgram,
   info: DebugReportInfo,
 ): void {
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    console.error("error", gl.getProgramInfoLog(shaderProgram) || "link failed", info);
-  } else {
-    let infoLog = gl.getProgramInfoLog(shaderProgram);
-    if (infoLog) {
-      if (infoLog.indexOf("WARN") >= 0) {
-        console.warn("warn", infoLog, info);
-      }
-    }
-    gl.validateProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
-      console.error("error", gl.getProgramInfoLog(shaderProgram) || "validation failed", info);
+  if (DEBUG) {
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      console.error("error", gl.getProgramInfoLog(shaderProgram) || "link failed", info);
     } else {
-      infoLog = gl.getProgramInfoLog(shaderProgram);
+      let infoLog = gl.getProgramInfoLog(shaderProgram);
       if (infoLog) {
         if (infoLog.indexOf("WARN") >= 0) {
           console.warn("warn", infoLog, info);
+        }
+      }
+      gl.validateProgram(shaderProgram);
+      if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+        console.error("error", gl.getProgramInfoLog(shaderProgram) || "validation failed", info);
+      } else {
+        infoLog = gl.getProgramInfoLog(shaderProgram);
+        if (infoLog) {
+          if (infoLog.indexOf("WARN") >= 0) {
+            console.warn("warn", infoLog, info);
+          }
         }
       }
     }
