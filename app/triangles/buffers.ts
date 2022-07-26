16fa706c0128503531de6ff3125589ae431c8c94
Salvatore Previti <roorback@gmail.com>
2022-07-26 10:57:17 +0100

    minor

---
+++ app/triangles/buffers.ts
@@ -0,0 +1,9 @@
+import { gl } from "../gl";
+
+export const positionsBuffer = gl.createBuffer();
+
+export const normalsBuffer = gl.createBuffer();
+
+export const colorsBuffer = gl.createBuffer();
+
+export const indexBuffer = gl.createBuffer();
