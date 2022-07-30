e80304dae29c078e28c103624ef0042f73d08aa0
Salvatore Previti <roorback@gmail.com>
2022-07-30 17:59:14 +0100

    wip

--- dev/builder/steps/build-swc-transform.ts
+++ dev/builder/steps/build-swc-transform.ts
@@ -43,7 +43,7 @@ export function rollupPluginSwcTransform(): PluginOption {
         configFile: false,
         filename: id,
         isModule: true,
-        minify: true,
+        minify: false,
         swcrc: false,
         jsc: {
           keepClassNames: false,
