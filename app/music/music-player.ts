df63e109b985283107a173880142247d62793128
Salvatore Previti <roorback@gmail.com>
2022-09-10 00:28:33 +0100

    wip

--- app/music/music-player.ts
+++ app/music/music-player.ts
@@ -23,180 +23,163 @@
  *
  */
 
-import { song_endPattern, song_rowLen, song_patternLen, song_songData } from "./song";
-import { osc_sin, osc_square, osc_saw, osc_tri } from "./oscillators";
+// Source code of the original player was manually modified to reduce bundle size.
+// This makes this version not compatible with default soundbox songs.
 
-//--------------------------------------------------------------------------
-// Private methods
-//--------------------------------------------------------------------------
+import type { song_songData } from "./song";
+import { song_endPattern, song_rowLen, song_patternLen } from "./song";
 
-// Array of oscillator functions
-const mOscillators = [osc_sin, osc_square, osc_saw, osc_tri];
-
-const getnotefreq = (n: number) => 0.003959503758 * 2 ** ((n - 128) / 12);
-
-const createNote = function (instr: ArrayElement<typeof song_songData>["i"], n: number, rowLen: number) {
-  const osc1 = mOscillators[instr[0]]!; // mOscillators[0|2]
-  const o1vol = instr[1];
-  const o1xenv = instr[3] / 32;
-  const osc2 = mOscillators[instr[4]]!; // mOscillators[0|1|3]
-  const o2vol = instr[5];
-  const o2xenv = instr[8] / 32;
-  const noiseVol = instr[9];
-  const attack = instr[10] * instr[10] * 4;
-  const sustain = instr[11] * instr[11] * 4;
-  const release = instr[12] * instr[12] * 4;
-  const expDecay = -instr[13] / 16;
-  let arp: number = instr[14];
-  const arpInterval = rowLen * 2 ** (2 - instr[15]);
-
-  const noteBuf = new Int32Array(attack + sustain + release);
-
-  // Re-trig oscillators
-  let c1 = 0;
-  let c2 = 0;
-
-  // Local variables.
-  let e: number;
-  let o1t: number;
-  let o2t: number;
-
-  // Generate one note (attack + sustain + release)
-  for (let j = 0, j2 = 0; j < attack + sustain + release; j++, j2++) {
-    if (j2 >= 0) {
-      // Switch arpeggio note.
-      arp = (arp >> 8) | ((arp & 255) << 4);
-      j2 -= arpInterval;
-
-      // Calculate note frequencies for the oscillators
-      o1t = getnotefreq(n + (arp & 15) + instr[2] - 128);
-      o2t = getnotefreq(n + (arp & 15) + instr[6] - 128) * (1 + 0.0008 * instr[7]);
-    }
-
-    // Envelope
-    e = 1;
-    if (j < attack) {
-      e = j / attack;
-    } else if (j >= attack + sustain) {
-      e = (j - attack - sustain) / release;
-      e = (1 - e) * 3 ** (expDecay * e);
-    }
-
-    // Add to (mono) channel buffer
-    noteBuf[j] =
-      (80 * // Oscillator 1
-        (osc1((c1 += o1t! * e ** o1xenv)) * o1vol +
-          // Oscillator 2
-          osc2((c2 += o2t! * e ** o2xenv)) * o2vol +
-          // Noise oscillator
-          (noiseVol ? (Math.random() * 2 - 1) * noiseVol : 0)) *
-        e) |
-      0;
-  }
-
-  return noteBuf;
-};
-
-//--------------------------------------------------------------------------
-// Private members
-//--------------------------------------------------------------------------
-
+const arpInterval = song_rowLen * 4;
 const mNumWords = song_rowLen * song_patternLen * (song_endPattern + 1) * 2;
 
+/** oscillator 0 */
+const osc_sin = (value: number) => Math.sin(value * Math.PI * 2);
+
+/** oscillator 1 */
+const osc_square = (value: number) => (value % 1 < 0.5 ? 1 : -1);
+
+/** oscillator 2 */
+const osc_saw = (value: number) => 2 * (value % 1) - 1;
+
+/** oscillator 3 */
+const osc_tri = (value: number) => {
+  const v2 = (value % 1) * 4;
+  return v2 < 2 ? v2 - 1 : 3 - v2;
+};
+
+// Array of oscillator functions
+const _oscillators = [osc_sin, osc_square, osc_saw, osc_tri] as const;
+
+const getnotefreq = (n: number) => 0.003959503758 * 2 ** ((n - 256) / 12);
+
 // Work buffer
-const mMixBuf = new Int32Array(mNumWords);
+const _audoMixBuf = new Int32Array(mNumWords);
 
-//--------------------------------------------------------------------------
-// Public methods
-//--------------------------------------------------------------------------
-
-//
 /**
  * Generate audio data for a single track/channel.
  * Have to be repeated from 0 to song_numChannels-1
  * @returns
  */
-export const soundbox_generate = (channel: number) => {
+export const soundbox_generate = (channel: ArrayElement<typeof song_songData>) => {
+  const [instrument, patterns, columns] = channel;
+
+  const createNote = function (note: number) {
+    const osc1 = _oscillators[instrument[0]]!; // mOscillators[0|2]
+    const o1vol = instrument[1];
+    const o1xenv = instrument[3] / 32;
+    const osc2 = _oscillators[instrument[4]]!; // mOscillators[0|1|3]
+    const o2vol = instrument[5];
+    const o2xenv = instrument[8] / 32;
+    const noiseVol = instrument[9];
+    const attack = instrument[10] ** 2 * 4;
+    const sustain = instrument[11] ** 2 * 4;
+    const release = instrument[12] ** 2 * 4;
+    const expDecay = -instrument[13] / 16;
+    let arp: number = instrument[14];
+
+    const noteBuf = new Int32Array(attack + sustain + release);
+
+    // Re-trig oscillators
+    let c1 = 0;
+    let c2 = 0;
+
+    // Local variables.
+    let e: number;
+    let o1t: number;
+    let o2t: number;
+
+    // Generate one note (attack + sustain + release)
+    for (let j1 = 0, j2 = 0; j1 < attack + sustain + release; j1++, j2++) {
+      if (j2 >= 0) {
+        // Switch arpeggio note.
+        arp = (arp >> 8) | ((arp & 255) << 4);
+        j2 -= arpInterval;
+
+        // Calculate note frequencies for the oscillators
+        o1t = getnotefreq(note + (arp & 15) + instrument[2]);
+        o2t = getnotefreq(note + (arp & 15) + instrument[6]) * (1 + 0.0008 * instrument[7]);
+      }
+
+      // Envelope
+      e = 1;
+      if (j1 < attack) {
+        e = j1 / attack;
+      } else if (j1 >= attack + sustain) {
+        e = (j1 - attack - sustain) / release;
+        e = (1 - e) * 3 ** (expDecay * e);
+      }
+
+      // Add to (mono) channel buffer
+      noteBuf[j1] =
+        (80 * // Oscillator 1
+          (osc1((c1 += o1t! * e ** o1xenv)) * o1vol +
+            // Oscillator 2
+            osc2((c2 += o2t! * e ** o2xenv)) * o2vol +
+            // Noise oscillator
+            (noiseVol ? (Math.random() * 2 - 1) * noiseVol : 0)) *
+          e) |
+        0;
+    }
+    return noteBuf;
+  };
+
   // Local variables
-  let i;
-  let j;
-  let p;
-  let row;
-  let col;
   let n;
-  let cp;
-  let k;
   let t;
-  let rsample;
-  let rowStartSample;
   let f;
 
   // Put performance critical items in local variables
   const chnBuf = new Int32Array(mNumWords);
-  const instr = song_songData[channel]!;
-  const rowLen = song_rowLen;
   const patternLen = song_patternLen;
 
   // Clear effect state
   let low = 0;
   let band = 0;
   let high;
-  let lsample;
-  let filterActive = false;
+  let filterActive: boolean | undefined;
 
   // Clear note cache.
   const noteCache = [];
 
   // Patterns
-  for (p = 0; p <= song_endPattern; ++p) {
-    cp = instr.p[p]!;
+  for (let p = 0, cp; p <= song_endPattern; ++p) {
+    cp = patterns[p]!;
 
     // Pattern rows
-    for (row = 0; row < patternLen; ++row) {
-      // Execute effect command.
-      // const cmdNo = cp ? instr.c[cp - 1]!.f[row] : 0;
-      // if (cmdNo) {
-      //   instr.i[cmdNo - 1] = instr.c[cp - 1]!.f[row + patternLen] || 0;
-
-      //   // Clear the note cache since the instrument has changed.
-      //   if (cmdNo < 17) {
-      //     noteCache = [];
-      //   }
-      // }
-
+    for (let row = 0; row < patternLen; ++row) {
       // Put performance critical instrument properties in local variables
-      const oscLFO = mOscillators[instr.i[16]]!;
-      const lfoAmt = instr.i[17] / 512;
-      const lfoFreq = 2 ** (instr.i[18] - 9) / rowLen;
-      const fxLFO = instr.i[19];
-      const fxFilter = instr.i[20];
-      const fxFreq = (instr.i[21] * 43.23529 * 3.141592) / 44100;
-      const q = 1 - instr.i[22] / 255;
-      const dist = instr.i[23] * 1e-5;
-      const drive = instr.i[24] / 32;
-      const panAmt = instr.i[25] / 512;
-      const panFreq = (6.283184 * 2 ** (instr.i[26] - 9)) / rowLen;
-      const dlyAmt = instr.i[27] / 255;
-      const dly = (instr.i[28] * rowLen) & ~1; // Must be an even number
+      const lfoAmt = instrument[17] / 512;
+      const lfoFreq = 2 ** (instrument[18] - 9) / song_rowLen;
+      const fxLFO = instrument[19];
+      const fxFilter = instrument[20];
+      const fxFreq = (instrument[21] * 43.23529 * 3.141592) / 44100;
+      const q = 1 - instrument[22] / 255;
+      const dist = instrument[23] * 1e-5;
+      const drive = instrument[24] / 32;
+      const panAmt = instrument[25] / 512;
+      const panFreq = (Math.PI * 2 ** (instrument[26] - 8)) / song_rowLen;
+      const dlyAmt = instrument[27] / 255;
+      const dly = (instrument[28] * song_rowLen) & ~1; // Must be an even number
 
       // Calculate start sample number for this row in the pattern
-      rowStartSample = (p * patternLen + row) * rowLen;
+      const rowStartSample = (p * patternLen + row) * song_rowLen;
 
       // Generate notes for this pattern row
-      for (col = 0; col < 4; ++col) {
-        n = cp ? instr.c[cp - 1]![row + col * patternLen] : 0;
+      for (let col = 0; col < 4; ++col) {
+        n = cp ? columns[cp - 1]![row + col * patternLen] : 0;
         if (n) {
-          const noteBuf = noteCache[n] || (noteCache[n] = createNote(instr.i, n, rowLen));
-          for (j = 0, i = rowStartSample * 2; j < noteBuf.length; j++, i += 2) {
+          const noteBuf = noteCache[n] || (noteCache[n] = createNote(n));
+          for (let j = 0, i = rowStartSample * 2; j < noteBuf.length; j++, i += 2) {
             chnBuf[i] += noteBuf[j]!;
           }
         }
       }
 
       // Perform effects for this pattern row
-      for (j = 0; j < rowLen; j++) {
+      for (let j = 0, rsample; j < song_rowLen; j++) {
+        let lsample = 0;
         // Dry mono-sample
-        k = (rowStartSample + j) * 2;
+        const k = (rowStartSample + j) * 2;
         rsample = chnBuf[k]!;
 
         // We only do effects if we have some sound input
@@ -204,18 +187,18 @@ export const soundbox_generate = (channel: number) => {
           // State variable filter
           f = fxFreq;
           if (fxLFO) {
-            f *= oscLFO(lfoFreq * k) * lfoAmt + 0.5;
+            f *= osc_sin(lfoFreq * k) * lfoAmt + 0.5;
           }
           f = 1.5 * Math.sin(f);
           low += f * band;
           high = q * (rsample - band) - low;
           band += f * high;
-          rsample = fxFilter == 3 ? band : fxFilter == 1 ? high : low;
+          rsample = fxFilter === 3 ? band : fxFilter === 1 ? high : low;
 
           // Distortion
           if (dist) {
             rsample *= dist;
-            rsample = rsample < 1 ? (rsample > -1 ? osc_sin(rsample * 0.25) : -1) : 1;
+            rsample = rsample < 1 ? (rsample > -1 ? osc_sin(rsample / 4) : -1) : 1;
             rsample /= dist;
           }
 
@@ -229,8 +212,6 @@ export const soundbox_generate = (channel: number) => {
           t = Math.sin(panFreq * k) * panAmt + 0.5;
           lsample = rsample * (1 - t);
           rsample *= t;
-        } else {
-          lsample = 0;
         }
 
         // Delay is always done, since it does not need sound input
@@ -247,8 +228,8 @@ export const soundbox_generate = (channel: number) => {
         chnBuf[k + 1] = rsample | 0;
 
         // ...and add to stereo mix buffer
-        mMixBuf[k] += lsample | 0;
-        mMixBuf[k + 1] += rsample | 0;
+        _audoMixBuf[k] += lsample | 0;
+        _audoMixBuf[k + 1] += rsample | 0;
       }
     }
   }
@@ -258,9 +239,8 @@ export const soundbox_generate = (channel: number) => {
 export const soundbox_createAudioBuffer = (audioContext: AudioContext) => {
   const buffer = audioContext.createBuffer(2, mNumWords / 2, 44100);
   for (let i = 0; i < 2; i++) {
-    const data = buffer.getChannelData(i);
-    for (let j = i; j < mNumWords; j += 2) {
-      data[j >> 1] = mMixBuf[j]! / 65536;
+    for (let j = i, data = buffer.getChannelData(i); j < mNumWords; j += 2) {
+      data[j >> 1] = _audoMixBuf[j]! / 65536;
     }
   }
   return buffer;
--- app/music/xxxaudio.ts
+++ app/music/xxxaudio.ts
@@ -1,4 +1,4 @@
-import { song_numChannels } from "./song";
+import { song_numChannels, song_songData } from "./song";
 import { soundbox_createAudioBuffer, soundbox_generate } from "./music-player";
 // import { song } from "./song";
 
@@ -7,10 +7,10 @@ export const makePlaye = () => {
     console.time("audio");
   }
 
-  const audioCtx = new window.AudioContext();
+  const audioCtx = new AudioContext();
 
   for (let i = 0; i < song_numChannels; ++i) {
-    soundbox_generate(i);
+    soundbox_generate(song_songData[i]!);
   }
 
   // const audioCtx = new AudioContext();
@@ -30,26 +30,4 @@ export const makePlaye = () => {
   if (DEBUG) {
     console.timeEnd("audio");
   }
-
-  // console.log("should play now");
-  // const source = audioCtx.createBufferSource();
-  // // set the buffer in the AudioBufferSourceNode
-  // source.buffer = audioBuffer;
-  // // connect the AudioBufferSourceNode to the
-  // // destination so we can hear the sound
-  // source.connect(audioCtx.destination);
-  // // start the source playing
-  // // source.start();
-  // source.start();
-  // // Fill the buffer with white noise;
-  // // just random values between -1.0 and 1.0
-  // for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-  //   // This gives us the actual ArrayBuffer that contains the data
-  //   const nowBuffering = myArrayBuffer.getChannelData(channel);
-  //   for (let i = 0; i < myArrayBuffer.length; i++) {
-  //     // Math.random() is in [0; 1.0]
-  //     // audio needs to be in [-1.0; 1.0]
-  //     nowBuffering[i] = Math.random() * 2 - 1;
-  //   }
-  // }
 };
