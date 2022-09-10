76156252c573f9eba5d0685dbf20f074824718e3
Salvatore Previti <roorback@gmail.com>
2022-09-10 02:55:11 +0100

    page

--- app/page.ts
+++ app/page.ts
@@ -82,7 +82,7 @@ export const initPage = () => {
 
   b3.onclick = () => {
     // eslint-disable-next-line no-alert
-    if (confirm("Delete saved game and restart?")) {
+    if (confirm("Delete game progress?")) {
       localStorage[LOCAL_STORAGE_SAVED_GAME_KEY] = "";
       location.reload();
     }
